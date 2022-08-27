d0981c272c71005419b0bda7d94628e99b9164b9
AyodejiSheen <aoluwasegun009@gmail.com>
2022-08-27 19:21:08 +0100

    newa

--- src/components/dashbaord/dashboard.js
+++ src/components/dashbaord/dashboard.js
@@ -1,7 +1,123 @@
- export const Dashboard = () => {
-    return(
+import box1 from '../../assets/box.png'
+import box2 from '../../assets/box2.png'
+import coin from '../../assets/coin.png'
+import cuate from '../../assets/caute.png'
+
+import { Link } from 'react-router-dom'
+
+
+export const Dashboard = () => {
+
+    return (
         <>
-        <p>This is the dashboard page</p>
+            <div className='md:flex justify-between gap-16 space-y-6 md:space-y-0'>
+                <div className='w-full lg:w-2/3 bg-yellow-50 rounded-3xl shadow-xl shadow-slate-100 md:h-44 py-6'>
+                    <div className='px-8 flex items-center md:gap-10 gap-6'>
+                        <div><img src={box1} alt="" className='md:w-48' /></div>
+                        <div className='flex flex-col lg:flex-row md:items-center space-y-8 md:space-y-0   '>
+                            <div className='space-y-2.5'>
+                                <h2 className='text-xl md:text-3xl font-medium'>Create Group</h2>
+                                <p className='text-sm md:text-base'>Lorem Ipsum dolor sit amet Lorem Ipsum dolor sit amet</p>
+                            </div>
+                            <div className='mx-0 md:mx-8'>
+                                <Link to="#" className='bg-teal-800 text-white px-10 md:py-3 py-2 rounded-full hover:bg-white hover:text-teal-800 border-2 border-teal-800'>Create</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='w-full md:w-1/3 bg-yellow-200 rounded-3xl shadow-xl shadow-yellow-100 h-44 py-6'>
+                    <div className='items-center gap-10 px-8 flex justify-between'>
+                        <div className='space-y-2.5'>
+                            <h2 className='text-3xl'>₦ 0</h2>
+                            <p className='text-base font-medium'>TOTAL BALANCE</p>
+                        </div>
+
+                        <div><img src={coin} alt="" className='w-32 md:w-36' /></div>
+                    </div>
+                </div>
+            </div>
+
+            {/* section 2 */}
+
+            <div className='md:flex justify-between gap-16 mt-16'>
+                <div className='w-full md:w-2/3 space-y-12'>
+                    <div>
+                        <h3 className='mb-6 font-medium text-slate-700'>OVERVIEW</h3>
+                        <div className='bg-white shadow-xl shadow-slate-200 rounded-3xl p-8 md:p-14'>
+                            <div className='flex md:gap-14'>
+                                <div className='w-1/2 border-r px-8'>
+                                    <div className='flex flex-col md:flex-row text-center items-center gap-5'>
+                                        <div className='bg-teal-100 rounded-full p-5 text-teal-800'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
+                                            </svg>
+                                        </div>
+                                        <div>
+                                            <h2 className='text-lg md:text-2xl'>0</h2>
+                                            <p className='text-sm md:text-base text-slate-700 font-medium'>COMPLETED STASHES</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='w-1/2 px-8'>
+                                    <div className='flex flex-col md:flex-row text-center items-center gap-5'>
+                                        <div className='bg-yellow-100 rounded-full p-5 text-red-800'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m0-10.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.75c0 5.592 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.75h-.152c-3.196 0-6.1-1.249-8.25-3.286zm0 13.036h.008v.008H12v-.008z" />
+                                            </svg>
+
+                                        </div>
+                                        <div>
+                                            <h2 className='text-lg md:text-2xl'>0</h2>
+                                            <p className='text-sm md:text-base text-slate-700 font-medium'>PENDING STASHES</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 className='mb-6 font-medium text-slate-700'>EXPLORE EVENTS</h3>
+                        <div className='bg-white shadow-xl shadow-slate-200 rounded-3xl py-16'>
+                            <div className='px-8 flex items-center gap-10'>
+                                <div><img src={box2} alt="" className='w-40' /></div>
+                                <div className=''>
+                                    <h2 className='font-semibold text-lg'>No available events</h2>
+                                    <p className='text-base'>There is no available events right now.</p>
+                                    <p className='text-base'>Take the lead. Create an event.</p>
+                                </div>
+                                <div className='mx-8'>
+                                    <Link to="#" className='bg-teal-800 text-white px-10 rounded-full py-3 hover:bg-white hover:text-teal-800 border-2 border-teal-800'>Create</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='w-1/3'>
+                    <div className='flex flex-wrap justify-between mb-6'>
+                        <h3 className='text-slate-700 font-medium'>YOUR GROUPS</h3>
+                        <p className='text-slate-700 font-medium'>See all</p>
+                    </div>
+
+                    <div className='bg-white shadow-xl shadow-slate-200 rounded-3xl'>
+                        <div className='px-8 py-28 text-center gap-10'>
+                            <div><img src={cuate} alt="" className='w-40 mx-auto mb-6' /></div>
+                            <div className=''>
+                                <h2 className='font-semibold text-lg'>No events yet</h2>
+                                <p className='text-base'>There is no available events right now.</p>
+                                <p className='text-base'>Take the lead. Create an event.</p>
+                            </div>
+                            <div className='mx-8 mt-8'>
+                                <Link to="#" className='bg-teal-800 text-white px-10 rounded-full py-3 hover:bg-white hover:text-teal-800 border-2 border-teal-800'>Explore</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
- }
+}
--- src/views/dashnav.js
+++ src/views/dashnav.js
@@ -24,16 +24,16 @@ export const Dashnav = () => {
 
             <div>
                 <div className="lg:flex flex-wrap justify-between">
-                    <div className={`z-20 fixed transform h-screen md:translate-x-0 inset-y-0 left-0 transition duration-300 ease-in-out overflow-y-auto w-80 bg-teal-800 px-12 py-20 text-white space-y-24 ${mobileNav ? "translate-x-0" : "-translate-x-full"}`}>
+                    <div className={`z-20 fixed transform h-screen lg:translate-x-0 inset-y-0 left-0 transition duration-300 ease-in-out overflow-y-auto w-80 bg-teal-800 px-12 py-20 text-white space-y-24 ${mobileNav ? "translate-x-0" : "-translate-x-full"}`}>
 
                         {/* hamburger close icon */}
-                        <div onClick={toggleNav} className="md:hidden ml-48">
+                        <div onClick={toggleNav} className="lg:hidden ml-48">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6">
                                 <path fill-rule="evenodd" d="M4.28 3.22a.75.75 0 00-1.06 1.06L8.94 10l-5.72 5.72a.75.75 0 101.06 1.06L10 11.06l5.72 5.72a.75.75 0 101.06-1.06L11.06 10l5.72-5.72a.75.75 0 00-1.06-1.06L10 8.94 4.28 3.22z" clip-rule="evenodd" />
                             </svg>
                         </div>
 
-                        <img src={Logo} className="hidden md:block close" alt="" />
+                        <img src={Logo} className="hidden lg:block close" alt="" />
 
                         <div>
                             <h5 className="mb-3 px-8">MENU</h5>
@@ -47,26 +47,27 @@ export const Dashnav = () => {
                                     </NavLink>
                                 </div>
 
-
                                 <div>
                                     <NavLink to="/" onClick={toggleNav} className={({ isActive }) => (isActive ? "text-teal-800 bg-white w-full rounded py-3 inline-flex space-x-3 items-center px-8" : "bg-none inline-flex space-x-3 py-3 px-8 text-white")} >
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                                        </svg> <span>Events</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+                                        </svg>
+                                        <span>Stash</span>
                                     </NavLink>
                                 </div>
 
                                 <NavLink to="/" onClick={toggleNav} className={({ isActive }) => (isActive ? "text-teal-800 bg-white w-full rounded py-3 inline-flex space-x-3 items-center px-8" : "bg-none inline-flex space-x-3 py-3 px-8 text-white")} >
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                                    </svg> <span>Community</span>
+                                    </svg> <span>Groups</span>
                                 </NavLink>
 
 
                                 <NavLink to="/" onClick={toggleNav} className={({ isActive }) => (isActive ? "text-teal-800 bg-white w-full rounded py-3 inline-flex space-x-3 items-center px-8" : "bg-none inline-flex space-x-3 py-3 px-8 text-white")} >
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                                    </svg> <span>Messages</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
+                                    </svg>
+                                    <span>History</span>
                                 </NavLink>
                             </div>
                         </div>
@@ -75,15 +76,17 @@ export const Dashnav = () => {
                             <h5 className="mb-3 px-8">ACCOUNT</h5>
                             <div className="space-y-3">
                                 <NavLink to="#" onClick={toggleNav} className={({ isActive }) => (isActive ? "inline-flex space-x-3 items-center text-teal-800 bg-white w-full rounded px-8 py-3" : "bg-none inline-flex space-x-3 py-3 px-8 text-white")}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                                    </svg> <span>Profile</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    </svg>
+                                    <span>Profile</span>
                                 </NavLink>
 
                                 <NavLink to="#" onClick={toggleNav} className={({ isActive }) => (isActive ? "inline-flex space-x-3 items-center text-teal-800 bg-white w-full rounded px-8 py-3" : "bg-none text-white")}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                                    </svg> <span>Log Out</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                                    </svg>
+                                    <span>Log Out</span>
                                 </NavLink>
                             </div>
                         </div>
@@ -100,17 +103,18 @@ export const Dashnav = () => {
                     </div>
 
 
-                    <div className="lg:ml-80 md:flex-1 w-full 2xl:px-36 2xl:py-10">
-                        <div className="flex flex-wrap items-center justify-between mx-8 my-6 lg:mr-0">
+                    <div className="lg:ml-80 md:flex-1 w-full py-8 lg:py-8 lg:px-12 2xl:px-36 2xl:py-10 bg-slate-100 h-full">
+                        <div className="flex flex-wrap items-center justify-between mx-8 lg:mr-0">
                             <div>
-                                <button onClick={toggleNav} className=" focus:border-2 p-1 mr-5 -ml-1 rounded-lg md:hidden  focus:shadow-outline-purple">
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                                <button onClick={toggleNav} className=" focus:border-2 p-1 mr-5 -ml-1 rounded-lg lg:hidden  focus:shadow-outline-purple">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
                                     </svg>
+
                                 </button>
                             </div>
 
-                            <div className="flex space-x-8 mr-8">
+                            <div className="flex space-x-8">
                                 <div>
                                     <Theme />
                                 </div>
@@ -125,7 +129,7 @@ export const Dashnav = () => {
                         </div>
 
 
-                        <div className="">
+                        <div className="px-8 lg:px-0 py-10 ">
                             <Outlet />
                         </div>
 
